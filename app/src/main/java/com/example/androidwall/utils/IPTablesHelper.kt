a002c47844ff4148a86964b74579d062036a74f5
PrimoDev23 <lexx.ps2711@gmail.com>
2021-03-24 20:41:49 +0100

    fix: Fix whitelist mode and rework cleanup on disable

--- app/src/main/java/com/example/androidwall/utils/IPTablesHelper.kt
+++ app/src/main/java/com/example/androidwall/utils/IPTablesHelper.kt
@@ -14,28 +14,30 @@ object IPTablesHelper {
 
     //Currently we only have whitelist mode and selected all by default
     //This is for testing purposes but will change later
-    fun applyRuleset(ruleset : RuleSet) {
+    fun applyRuleset(ruleset: RuleSet) {
         //Get a root shell
         val shell = Runtime.getRuntime().exec("su")
 
         //Only apply rules if firewall is not disabled
-        if(ruleset.enabled) {
+        if (ruleset.enabled) {
             //We are always setting up the chain again since we can't really check for errors
             //If operation succeed error stream can't be read
             setupChain(shell)
 
             if (ruleset.mode == FirewallMode.WHITELIST) {
                 //Default to drop for whitelist
-                runCommand(
-                    shell,
-                    "iptables -P OUTPUT DROP"
-                )
+                runCommand(shell, "iptables -P OUTPUT DROP")
+
+                //Set some base rules (those are needed to make internet work for whitelisted apps)
+                runCommand(shell, "iptables -A $CHAINNAME -m owner --uid-owner wifi -j ACCEPT")
+                runCommand(shell, "iptables -A $CHAINNAME -m owner --uid-owner dhcp -j ACCEPT")
+                runCommand(shell, "iptables -A $CHAINNAME -m owner --uid-owner root -j ACCEPT")
+
+                //Accept all outgoing connections to loopback
+                runCommand(shell, "iptables -A OUTPUT -o lo -j ACCEPT")
             } else {
                 //Default to accept on blacklist
-                runCommand(
-                    shell,
-                    "iptables -P OUTPUT ACCEPT"
-                )
+                runCommand(shell, "iptables -P OUTPUT ACCEPT")
             }
 
             val policy = if (ruleset.mode == FirewallMode.WHITELIST) "ACCEPT" else "DROP"
@@ -65,35 +67,36 @@ object IPTablesHelper {
                     )
                 }
             }
-        }else{
-            //Flush rules
-            runCommand(shell, "iptables -F $CHAINNAME")
-
-            //Accept all connections in output
-            runCommand(shell, "iptables -P OUTPUT ACCEPT")
+        } else {
+            cleanupChain(shell)
         }
 
         runCommand(shell, "exit")
     }
 
-    private fun setupChain(shell : Process){
+    private fun setupChain(shell: Process) {
         //Remove the chain from output if possible
         runCommand(shell, "iptables -D OUTPUT -j AndroidWallChain")
-
-        //Remove accepting all outgoing connections to loopback
-        runCommand(shell, "iptables -D OUTPUT -o lo -j ACCEPT")
 
         //Create our chain
         runCommand(shell, "iptables -N $CHAINNAME")
 
-        //Even if we checked we create a new chain, just flush it in case
+        //Always flush here
         runCommand(shell, "iptables -F $CHAINNAME")
 
         //Connect output to our chain
         runCommand(shell, "iptables -A OUTPUT -j $CHAINNAME")
+    }
 
-        //Accept all outgoing connections to loopback
-        runCommand(shell, "iptables -A OUTPUT -o lo -j ACCEPT")
+    private fun cleanupChain(shell: Process) {
+        //Remove the chain from output if possible
+        runCommand(shell, "iptables -D OUTPUT -j AndroidWallChain")
+
+        //Remove the chain
+        runCommand(shell, "iptables -X $CHAINNAME")
+
+        //Accept all connections in output (default)
+        runCommand(shell, "iptables -P OUTPUT ACCEPT")
     }
 
     private fun runCommand(shell: Process, command: String) {
